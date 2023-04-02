9a74929a4394fd56e52ced981eba8c95dd6ead81
Bloo <91748368+911-Bulat-Jaclina-Iana@users.noreply.github.com>
2023-04-02 14:20:33 +0300

    Added part of frontend

---
+++ vite-project/src/components/botanicalGarden/BotanicalGardenShowAll.tsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+import BotanicalGarden from "../../models/BotanicalGarden";
+
+export const BotanicalGardenShowAll = () => {
+  const [botanicalGarden, setBotanicalGarden] = useState([]);
+
+  useEffect(() => {
+  fetch("http://ec2-13-53-243-233.eu-north-1.compute.amazonaws.com/api/plant/")
+  .then(res => res.json())
+  .then(data => {
+    setBotanicalGarden(data)});
+  },[]);
+
+  return (
+    <div className="App">
+      <h1>Botanical Garden List</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>People capacity</th>
+            <th>Plant capacity</th>
+            <th>Location</th>
+          </tr>
+        </thead>
+        <tbody>
+        {botanicalGarden.map((botanicalGarden: BotanicalGarden, index) => {
+            return (
+              <tr key={index}>
+                <td>{index}</td>
+                <td>{botanicalGarden.bgName}</td>
+                <td>{botanicalGarden.bgCapacity}</td>
+                <td>{botanicalGarden.bgPlants}</td>
+                <td>{botanicalGarden.bgLocation}</td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+
+    </div>
+  )
+
+};
+  
